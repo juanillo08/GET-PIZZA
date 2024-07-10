9519cb278bee3f201c2629cf3245716bbf4fe23d
juanillo <juanillojohnmichael7@gmail.com>
2024-07-10 15:14:29 +0800

    Pizaa

--- dist/js/slider.js
+++ dist/js/slider.js
@@ -1,5 +1,13 @@
-const { Container } = require("postcss");
-
+// SLIDER FOR CLIENTS REVIEW 
 var slider = tns({
-    Container: ".feedback__slider"
-})
+    container: ".review__slider",
+    items: 1,
+    speed:  400,
+    gutter: 100,
+    slideBy:  "page",
+    autoplay: true,
+    nav: false,
+    navPosition: "bottom",
+    controls: false,
+    autoplayButtonOutput: false,
+});
